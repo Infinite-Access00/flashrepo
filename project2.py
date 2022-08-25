97ffbe9292de73f7f420896334fcab516052e20f
infiniteaccess <distinctionteam08@gmail.com>
2022-08-25 23:47:22 +0000

    this is today's project

---
+++ project2.py
@@ -0,0 +1,13 @@
+#!/bin/python
+
+a=Anthony
+b=Nicholas
+c=Simon
+echo "Let's begin the meeting. $a will begin recording"
+echo $b will take notes in the meeting. $c will translate.
+echo Welcome $USER
+echo the current size is $HISTSIZE
+echo today is 
+date
+
+

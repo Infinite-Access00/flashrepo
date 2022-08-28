cc35fb34e02b0f4c51ffc258b981da3816ad33a7
CKay925 <ckay925@gmail.com>
2022-08-28 20:00:19 +0000

    distinction.py script was modified

--- distinction.py
+++ distinction.py
@@ -1,1 +1,7 @@
-This is a practice python script
+#This is a practice python script
+#Use print for python scripts and echo for shell scripts
+#This script describes a Class29 Team
+print ('Class29 Team 8 is named Distinction')
+print ('Distinction strives for excellence')
+print ('Team Distinction will work together to reach our maximum potential')
+
